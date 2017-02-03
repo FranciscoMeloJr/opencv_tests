6ddb1ad374f392ef22db8d97099f9df4b276183b
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2017-02-03 01:24:23 -0500

    adding the Parser tests

--- tests/opencv-test.cpp
+++ tests/opencv-test.cpp
@@ -103,63 +103,69 @@ void help(String filename ){
         return;
 
 }
+//Keys:
+const char* keys =
+{
+    "{help h||}{@image|obama.jpg|input image file}"
+};
 //Main:
 int main( int argc, char** argv )
 {
     try{
-    cv::CommandLineParser parser(argc, argv, "{help h||}");
-    parser.about("OpenCv Tests v1.0.0");
-    cv::String filename;
-    filename = { "{ filename h| |}" };
-    int times = parser.get<int>("t");
-    if (parser.has("help"))
-    {
-            help(filename);
+
+        cv::CommandLineParser parser(argc, argv, keys);
+        parser.about("OpenCv Tests v1.0.0");
+        cv::String filename;
+        filename = { "{ filename h| |}" };
+        if (parser.has("help"))
+        {
+                help(filename);
+                return 0;
+        }
+
+        String command = argv[1];
+
+        String filename = argv[2];
+        int counter = NULL;
+        if(argv[3]!=NULL){
+            counter = times;
+        }
+        clock_t elapsed_time = NULL;
+
+        if(command.compare("display_image") == 0 ){
+                cout << "<display_image>" << endl;
+                elapsed_time = display(filename, counter);
+        }
+        if(command.compare("hough_lines") == 0 ){
+                cout <<"<hough_lines>" << endl;
+                elapsed_time = houghlines(filename, counter);
+        }
+
+        if(command.compare("face_detection") == 0 ){
+                cout <<"<face_detection>" << endl;
+                elapsed_time = face_detection(filename, counter);
+        }
+        if(command.compare("version") == 0 ){
+            cout << "\tUsing OpenCV version " << CV_VERSION << "\n" << endl;
+        }
+        if(command.size() == 0 ){
+            cout << command << " command not found" << endl;
             return 0;
-    }
+        }
 
-    String command = argv[1];
-
-    //String filename = argv[2];
-    int counter = NULL;
-    if(argv[3]!=NULL){
-        counter = times;
-    }
-    clock_t elapsed_time = NULL;
-
-    if(command.compare("display_image") == 0 ){
-            cout << "<display_image>" << endl;
-            elapsed_time = display(filename, counter);
-    }
-    if(command.compare("hough_lines") == 0 ){
-            cout <<"<hough_lines>" << endl;
-            elapsed_time = houghlines(filename, counter);
-    }
-
-    if(command.compare("face_detection") == 0 ){
-            cout <<"<face_detection>" << endl;
-            elapsed_time = face_detection(filename, counter);
-    }
-    if(command.compare("version") == 0 ){
-        cout << "\tUsing OpenCV version " << CV_VERSION << "\n" << endl;
-    }
-    if(command.size() == 0 ){
-        cout << command << " command not found" << endl;
-        return 0;
-    }
-
-    if(counter != NULL){
-        std::cout << command << " elapsed time: " << elapsed_time << " ns in" << counter << " times "<< std::endl;
-    }
-    else{
-        std::cout << command << " elapsed time: " << elapsed_time << " ns in 100 times" << std::endl;
-    }
+        if(counter != NULL){
+            std::cout << command << " elapsed time: " << elapsed_time << " ns in" << counter << " times "<< std::endl;
+        }
+        else{
+            std::cout << command << " elapsed time: " << elapsed_time << " ns in 100 times" << std::endl;
+        }
 
     }
     catch (int e)
      {
         cout << "An exception occurred. Exception Nr. " << e << '\n';
      }
+
     return 0;
 }
 
