5b2a19b3e7633722706565051246a787b8d186e7
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2017-03-02 23:32:17 -0500

    python changes for event

--- newTest/bashTest.sh
+++ newTest/bashTest.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-function PF(){
+function t(){
 echo $1$2
   for i in `seq 1 $1`
   do
      echo $i
-     tracePF $i $2
+     trace $i $2
   done
 
 }
 
-function tracePF(){
+function trace(){
 
   a='output=/tmp/ust-traces-'
-  b=$2
   c='-pf-'
   d=$a$b$c
   e=$d$1
@@ -23,8 +22,8 @@ function tracePF(){
   #./opencv_example 
   lttng enable-event -u sched_switch
   lttng add-context -u -t perf:thread:page-fault
-  #lttng add-context -u -t perf:thread:cache-misses 
-  #lttng add-context -u -t perf:thread:instructions 
+  lttng add-context -u -t perf:thread:cache-misses 
+  lttng add-context -u -t perf:thread:instructions 
 
   lttng start
 
@@ -39,10 +38,10 @@ then
    echo 'All';
    echo 'inline';
 
-   qtd=1
+   qtd=10
    data="../data/500x500.jpg"
 
-   PF $qtd $data
+   t $qtd $data
    
 fi
 
